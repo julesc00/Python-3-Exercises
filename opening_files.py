efd64032e57010542b41795b0646a122e8b01822
julesc00 <julesc00@hotmail.com>
2019-06-19 12:40:48 -0500

    Second commit
    Working with file reading and writing to files.

--- opening_files.py
+++ opening_files.py
@@ -1,5 +1,12 @@
+import json
+# import _pickle as pickle
+# Use json for dicts and sets creation to files.
+# Use pickle fo serialization
+
+
 new_text = "This is a new line of text.\n"
 text2 = "Another line of text to append.\n"
+
 l = ["Line 1", "Line 2", "Line 3"]
 family = {"Claudia": "wife",
           "Caricia": "daughter",
@@ -12,11 +19,19 @@ family = {"Claudia": "wife",
           "Benito": "son",
           "Jesusito": "son"}
 
-file = open("text.txt", 'w')
+file = open("text.txt", 'a')
 file.write(new_text + text2) # it worked
-for item, relationship  in family:
-    file.write(item + relationship + "\n")
+
+# to append is 'a'
+# with open('text.txt', 'w') as file:
+#     file.write(json.dumps(family)) # use json loads to do reverse.
+
+# since file is already open:
+file.write(json.dumps(family) + "\n")
+
 for item in l:
     file.write(item + "\n")
 
+# file.write(pickle.dumps(family)) # Use pickle 'loads' to do reverse.
+# Couldn't use it
 file.close()
